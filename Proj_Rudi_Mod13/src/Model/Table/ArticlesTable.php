5ec1d75129bfade054e474ffe8c4379b4e31fd17
Rudi Vicente <rudybenfica72@gmail.com>
2024-02-01 11:43:17 +0000

    Create ArticlesTable.php

---
+++ Proj_Rudi_Mod13/src/Model/Table/ArticlesTable.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Model\Table;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+class ArticlesTable extends Table
+{
+public function initialize(array $config) :void
+{ $this->addBehavior('Timestamp'); }
+public function validationDefault(Validator $validator): Validator
+{ $validator
+->notEmpty('title')
+->notEmpty('body');
+return $validator; }
+}
+?>
