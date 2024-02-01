264247429b3029ac36efd75b0462dc6d869072c8
Rudi Vicente <rudybenfica72@gmail.com>
2024-02-01 12:01:56 +0000

    Update view.php

--- Proj_Rudi_Mod13/templates/Articles/view.php
+++ Proj_Rudi_Mod13/templates/Articles/view.php
@@ -1,5 +1,5 @@
 <html>
-<h1><?= h($article->Marca) ?></h1>
+<h1><?= h($article->Marca) ?></h1> <!-- Aqui vai mostrar os dados da BD -->
 <p><?= h($article->Modelo) ?></p>
 <p><?= h($article->body) ?></p>
 </html>
