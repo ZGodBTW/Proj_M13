217b14a505ab0e7571601518d62e615f1a2987ba
Rudi Vicente <rudybenfica72@gmail.com>
2024-02-01 12:17:59 +0000

    Update edit.php

--- Proj_Rudi_Mod13/templates/Articles/edit.php
+++ Proj_Rudi_Mod13/templates/Articles/edit.php
@@ -2,7 +2,8 @@
 <h1>Edit Article</h1>
 <?php
 echo $this->Form->create($article);
-echo $this->Form->input('title');
+echo $this->Form->input('Marca');
+echo $this->Form->input('Modelo');
 echo $this->Form->input('body', ['rows' => '3']);
 echo $this->Form->button(__('Salvar artigo')); echo $this->Form->end();
 ?>
