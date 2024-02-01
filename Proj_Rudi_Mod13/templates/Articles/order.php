9e6472e385c41f33650b4120a113e3d0e211707e
Rudi Vicente <rudybenfica72@gmail.com>
2024-02-01 13:00:11 +0000

    adicionado view order

---
+++ Proj_Rudi_Mod13/templates/Articles/order.php
@@ -0,0 +1,26 @@
+<html>
+<h1>Blog articles - ordenar alfabeticamente</h1>
+<p><?= $this->Html->link('Voltar atras', ['action' => 'index']) ?></p>
+<table>
+    <tr>
+        <th><?= $this->Paginator->sort('Id') ?></th>
+        <th>Marca</th>
+        <th>Modelo</th>
+        <th>Ações</th>
+    </tr>
+    <?php foreach ($articles as $article): ?>
+<tr>
+<td><?= $article->id ?></td>
+<td> <?= $article->Marca?> </td>
+<td> <?= $this->Html->link($article->Modelo, ['action' => 'view', $article->id]) ?> </td><!-- Quando Carregamos no modelo do carro abre a informacão dele -->
+
+<td>
+<?= $this->Form->postLink( 'Deletar ', ['action' => 'delete', $article->id], ['confirm' => 'Tem certeza?']) ?>
+
+<?= $this->Html->link(' Edit', ['action' => 'edit', $article->id]) ?>
+</td>
+</tr>
+<?php endforeach; ?>
+</table>
+</table>
+</html>
